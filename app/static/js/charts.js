109e8e2704b44eb4312ebd754447e07d7dce89d0
Anastasia Lee <alee51@stuy.edu>
2025-04-08 11:48:08 -0400

    update

--- app/static/js/charts.js
+++ app/static/js/charts.js
@@ -15,27 +15,5 @@ document.addEventListener("DOMContentLoaded", function(){
   var chart0 = new ApexCharts(document.querySelector("#chart0"), options0);
 
   chart0.render();
-  
-  var options1 = {
-    series: [44, 55, 13, 43, 22],
-    chart: {
-      width: 380,
-      type: 'pie',
-    },
-    labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
-    responsive: [{
-      breakpoint: 480,
-      options: {
-        chart: {
-          width: 200
-        },
-        legend: {
-          position: 'bottom'
-        }
-      }
-    }]
-  };
-  
-  var chart1 = new ApexCharts(document.querySelector("#chart"), options1);
-  chart1.render();
 });
+
